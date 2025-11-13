74576ce822eac8132e86fd7dedafdcb31ad33827
Kauã Barbosa <2023257@student.cct.ie>
2025-11-13 14:55:28 +0000

    Smart contract set up

--- solidityBlockChain/BusRide.sol
+++ solidityBlockChain/BusRide.sol
@@ -1,3 +1,16 @@
-Bus Ride APP{
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
 
-};
+contract SimpleStorage{
+    // State variables and functions will go here
+    uint256 private storedNumber;
+
+    function store(uint256 _number) public {
+        storedNumber = _number;
+    }
+
+    function retrieve() public view returns (uint256){
+        return storedNumber;
+    }
+    
+}
